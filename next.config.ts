301f6a4ecf4f0defe9a35ac0230e8ea6680399da
zack-dev-cm <zack@carbmanager.com>
2024-11-07 17:25:37 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,10 @@
 // next.config.ts
+
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
+  // Include any other Next.js configuration options here
 };
 
 export default nextConfig;
