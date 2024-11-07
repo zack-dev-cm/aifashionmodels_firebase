2f809c10f5fcefc4c38a80d6db80ae80f3146662
zack-dev-cm <zack@carbmanager.com>
2024-11-07 17:12:51 +0400

    add out

---
+++ out/_next/static/chunks/app/page-beab92c3c58ee97f.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{4603:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,8173,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[173,130,215,744],()=>s(4603)),_N_E=e.O()}]);
